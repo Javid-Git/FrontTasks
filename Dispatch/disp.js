f9a672bc0516c15176cbc1d5067162db525b021d
Javid-Git <javidaal@code.edu.az>
2022-04-25 01:45:52 +0400

    select funct but no dispatch

--- Dispatch/disp.js
+++ Dispatch/disp.js
@@ -8,11 +8,13 @@
 
 // let reload = new Event('reload', {bubbles: true});
 
-var elements = document.getElementsByTagName("*");
+var strongs = document.getElementsByTagName("*");
+var s = window.getSelection();
 
-var clickEvent  = document.createEvent ('MouseEvents');
-clickEvent.initEvent ('click', true, true);
-for (var i=0; i < elements.length; i++) 
-{    
-    elements[i].dispatchEvent (clickEvent);
+if(s.rangeCount > 0) s.removeAllRanges();
+
+for(var i = 0; i < strongs.length; i++) {
+   var range = document.createRange();
+   range.selectNode(strongs[i]);
+   s.addRange(range);
 }
